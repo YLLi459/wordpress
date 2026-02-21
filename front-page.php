9b9286e9a0706a22b3dc1b8c1aefd362d3f13275
YLLi459 <yllaliuy1@gmail.com>
2026-02-21 14:42:27 +0100

    wordpress
    wordpress

---
+++ front-page.php
@@ -0,0 +1,95 @@
+<?php get_header();?>
+    <main>
+        <section class='hero'>
+            <img class='hero-banner'
+            src='<?php echo esc_url(get_theme_file_url('images/foto3.png'));?>'
+            alt='site banner'>
+        </section>
+
+        <section class='features'>
+            <br>
+            <h2 id ='heading'>Why Choosing Us?</h2>
+            <br>
+
+            <ul>
+                <li>
+                    <img src="<?php echo esc_url(get_theme_file_url('images/foto1.png'));?>"
+                    alt='Foto1'>
+                    <span>Project Based Learning</span>
+                </li>
+
+                <li>
+                    <img src="<?php echo esc_url(get_theme_file_url('images/foto2.png'));?>"
+                    alt='Foto1'>
+                    <span>Instructors who inspire</span>
+                </li>
+
+                <li>
+                    <img src="<?php echo esc_url(get_theme_file_url('images/foto3.png'));?>"
+                    alt='Foto1'>
+                    <span>Future-Ready</span>
+                </li>
+
+                   <li>
+                    <img src="<?php echo esc_url(get_theme_file_url('images/foto4.png'));?>"
+                    alt='Foto1'>
+                    <span>Online or in-person</span>
+                </li>
+            </ul>
+</section>
+<br>
+
+
+<ul class='curricula-grid'>
+    <li class='card'>
+        <h3 class='card-title'>
+            <span class='label'>Curriculum for the group age</span>
+            <span class='age'>8-11 Years </span>
+        </h3>
+        <ul class='card-points'>
+            <li>CONTENT: Learning the logic and basics cocepts of programming </li>
+            <li>Period: September-July(10 Months/Academy Year)</li>
+            <li>Intensity: 3 Academic hours per week</li>
+            <li>Group: 8-16 Students</li>
+            <li>Method: Online or physically</li>
+         </ul>
+          </li> 
+
+
+
+
+          <li class='card'>
+        <h3 class='card-title'>
+            <span class='label'>Curriculum for the group age</span>
+            <span class='age'>11-15 Years </span>
+        </h3>
+        <ul class='card-points'>
+            <li>CONTENT: Learning the logic and basics cocepts of programming </li>
+            <li>Period: September-July(10 Months/Academy Year)</li>
+            <li>Intensity: 3 Academic hours per week</li>
+            <li>Group: 8-16 Students</li>
+            <li>Method: Online or physically</li>
+         </ul>
+          </li> 
+
+
+
+
+
+          <li class='card'>
+        <h3 class='card-title'>
+            <span class='label'>Curriculum for the group age</span>
+            <span class='age'>15-18 Years </span>
+        </h3>
+        <ul class='card-points'>
+            <li>CONTENT: Learning the logic and basics cocepts of programming </li>
+            <li>Period: September-July(10 Months/Academy Year)</li>
+            <li>Intensity: 3 Academic hours per week</li>
+            <li>Group: 8-16 Students</li>
+            <li>Method: Online or physically</li>
+         </ul>
+          </li> 
+
+</section>
+</main>
+<?php get_footer(); ?>
